008f19f7d6bc2bcf88ab719e91b938e5a48a5c6a
Tomas Alabes <tomas.alabes@oracle.com>
2017-01-23 14:41:31 -0800

    Tried in oj-webpack and worked, let's review it

--- index.js
+++ index.js
@@ -2,32 +2,33 @@
 
 const path = require('path');
 const fs = require('fs');
+const ConcatSource = require('webpack-sources').ConcatSource;
 
 function OjPlugin(options) {
   // Configure your plugin with options...
 }
 
-OjPlugin.prototype.apply = function(compiler) {
+OjPlugin.prototype.apply = function (compiler) {
   var self = this;
   var ojCore = "ojs/ojcore";
   var folder = compiler.options.context;
 
   var ojCore = path.join(folder, "bower_components/oraclejet/dist/js/libs/oj/debug/ojcore.js");
+  var originalOjCore = fs.readFileSync(ojCore, 'utf8');
 
-  fs.readFile(ojCore, 'utf8', function (err, data) {
-    if (err) {
-      throw err;
-    }
-    fs.writeFileSync(ojCore, data.replace("require(requestedBundles,", "myUniqueFunctionToReplaceInOjCore(requestedBundles,"));
+  fs.writeFileSync(ojCore, originalOjCore.replace("require(requestedBundles,", "myUniqueFunctionToReplaceInOjCore(requestedBundles,"));
 
-    compiler.plugin('done', function (stats) {
-      var outputPath = compiler.outputPath;
-      Object.keys(stats.compilation.assets).filter(asset => asset.match(/\.js$/) !== null)
-        .map(bundle => path.join(outputPath, bundle))
-        .forEach(function(bundle){
-          var newOutput = fs.readFileSync(bundle, 'utf8').replace("myUniqueFunctionToReplaceInOjCore", "require");
-          fs.writeFileSync(bundle, newOutput);
+  compiler.plugin("compilation", function (compilation) {
+    //the main compilation instance
+    //all subsequent methods are derived from compilation.plugin
+    compilation.plugin("optimize-chunk-assets", function (chunks, callback) {
+      chunks.forEach(function (chunk) {
+        chunk.files.forEach(function (file) {
+          compilation.assets[file] = new ConcatSource(compilation.assets[file].source().replace("myUniqueFunctionToReplaceInOjCore", "require"));
         });
+      });
+      fs.writeFileSync(ojCore, originalOjCore);
+      callback();
     });
   });
 
