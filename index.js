9da499155597d66780869dd9b66e908adfa69306
Tomas Alabes <tomasAlabes@users.noreply.github.com>
2017-12-11 10:19:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,39 +2,37 @@ const path = require('path');
 const fs = require('fs');
 const ConcatSource = require('webpack-sources').ConcatSource;
 
-const defaultOjCorePath = "bower_components/oraclejet/dist/js/libs/oj/debug/ojcore.js";
+const defaultOjCorePath = "node_modules/oraclejet/dist/js/libs/oj/debug/ojcore.js";
 
-function OjPlugin(options) {
-  // Configure plugin with options...
-  this.ojCorePath = defaultOjCorePath;
-  
-  if (options) {
-    this.ojCorePath = options.ojCorePath || defaultOjCorePath;
+module.exports = class ojPlugin {
+  constructor(options) {
+    this.options = options;
+    this.ojCorePath = defaultOjCorePath;
+    
+    if (options) {
+      this.ojCorePath = options.ojCorePath || defaultOjCorePath;
+    }
   }
-}
-
-OjPlugin.prototype.apply = function (compiler) {
-  var folder = compiler.options.context;
-  var ojCore = path.join(folder, this.ojCorePath);
-  var originalOjCore = fs.readFileSync(ojCore, 'utf8');
-
-  fs.writeFileSync(ojCore, originalOjCore.replace("require(requestedBundles,", "myUniqueFunctionToReplaceInOjCore(requestedBundles,"));
-
-  compiler.plugin("compilation", function (compilation) {
-    //the main compilation instance
-    //all subsequent methods are derived from compilation.plugin
-    compilation.plugin("optimize-chunk-assets", function (chunks, callback) {
-      //ToDo this seems a pretty non-performant approach, need to figure out how to improve it
-      chunks.forEach(function (chunk) {
-        chunk.files.forEach(function (file) {
-          compilation.assets[file] = new ConcatSource(compilation.assets[file].source().replace("myUniqueFunctionToReplaceInOjCore", "require"));
+  apply(compiler) {
+    var folder = compiler.options.context;
+    var ojCore = path.join(folder, this.ojCorePath);
+    var originalOjCore = fs.readFileSync(ojCore, 'utf8');
+  
+    fs.writeFileSync(ojCore, originalOjCore.replace("require(requestedBundles,", "myUniqueFunctionToReplaceInOjCore(requestedBundles,"));
+  
+    compiler.plugin("compilation", function (compilation) {
+      //the main compilation instance
+      //all subsequent methods are derived from compilation.plugin
+      compilation.plugin("optimize-chunk-assets", function (chunks, callback) {
+        //ToDo this seems a pretty non-performant approach, need to figure out how to improve it
+        chunks.forEach(function (chunk) {
+          chunk.files.forEach(function (file) {
+            compilation.assets[file] = new ConcatSource(compilation.assets[file].source().replace("myUniqueFunctionToReplaceInOjCore", "require"));
+          });
         });
+        fs.writeFileSync(ojCore, originalOjCore);
+        callback();
       });
-      fs.writeFileSync(ojCore, originalOjCore);
-      callback();
     });
-  });
-
+  }
 };
-
-module.exports = OjPlugin;
